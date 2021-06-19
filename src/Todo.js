8bad33b88cc932c3c538010fd069c71e13327ce5
elmedinkarisik <edo.karisik1@gmail.com>
2021-06-19 23:07:24 +0200

    Edit Todo component

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,20 @@
+import {
+  ListItem,
+  ListItemText,
+  List,
+  ListItemAvatar,
+} from "@material-ui/core";
+import "./Todo.css";
 import React from "react";
 
 function Todo(props) {
   return (
-    <div>
-      <li>{props.text}</li>
-    </div>
+    <List className="todo__list">
+      <ListItem>
+        <ListItemAvatar />
+        <ListItemText primary={props.text} secondary="Deadline" />
+      </ListItem>
+    </List>
   );
 }
 
