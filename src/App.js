8e1095d132f85a8276919d5a3300fb5caa7d418e
elmedinkarisik <edo.karisik1@gmail.com>
2021-06-19 23:37:18 +0200

    Add timestamp

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 import "./App.css";
 import Todo from "./Todo";
 import db from "./firebase";
+import firebase from "firebase";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
 
   useEffect(() => {
-    db.collection("todos").onSnapshot((snapshot) => {
-      setTodos(snapshot.docs.map((doc) => doc.data().todo));
-    });
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(snapshot.docs.map((doc) => doc.data().todo));
+      });
   }, []);
 
   const addTodo = (event) => {
@@ -19,6 +22,7 @@ function App() {
 
     db.collection("todos").add({
       todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
     });
 
     setInput("");
